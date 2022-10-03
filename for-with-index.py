1db6eb5c3a2b10dd0815974e1ee9c9e167946c0b
allineleal <alline94@live.com>
2022-10-03 19:59:31 -0300

    example using enumerate

--- for-with-index.py
+++ for-with-index.py
@@ -1,3 +1,8 @@
 flowers = ['Rose', 'Jasmine', 'Orchid', 'Sunflower', 'Daisy']
 for index in range(len(flowers)):
     print('Flowers {}: {}'.format((index+1), flowers[index]))
+    
+print('\n') #example using enumerate
+for index, flower in enumerate(flowers, start=1):
+    print('Flowers {}: {}'.format((index), flower))
+  
