ca98add3a128f5a8563063bdb5355634e5b5332e
allineleal <alline94@live.com>
2022-10-03 20:08:35 -0300

    ternary with or

--- condition-ternary-operator.py
+++ condition-ternary-operator.py
@@ -5,3 +5,6 @@ print(word)
 passed = True
 word = ("try again", "conglatulation")[passed]  # ternary with tuple
 print(word)
+
+passed = False
+print(passed and "congratulation" or "try again")  # ternary with or
